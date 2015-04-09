ebd04b3441b0ac4dae5b671c5d68edee5aca1145
Susan VanderPlas <srvanderplas@gmail.com>
2015-04-09 03:00:05 -0500

    CRON Auto-Commit after script runs

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /home/susan/Documents/R\ Projects/AuburnWeather
+cd /home/susan/Programming/Rprojects/AuburnWeather
 R CMD BATCH ./ScrapeWeather.R
 git commit -a -m "CRON Auto-Commit after script runs"
 git push
